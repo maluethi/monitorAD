fa10f92de045dd4cfed0f9cd40cfbaf16d6cd121
matthias <matthias.luethi@lhep.unibe.ch>
2015-01-14 12:42:16 -0600

    Added comments and small restructuring of the script.

--- monitor.py
+++ monitor.py
@@ -1,15 +1,11 @@
-#Clock out word1 bit by bit on pin 19
-#Data valid when clock (pin 23) goes from L to H
-#Clock in 12 bits on pin 21
-#Data valid when clock (pin 23) goes from L to H
-#Work out decimal value based on position & value of bit
-#X = position of bit
-#Bit = 0 or 1
-#Value = value of that bit
-#anip = running total
-#Print x, bit, value, anip
-#Chip enable high
-#Work out and print voltage
+# author: matthias luethi
+# Script to display A/D converted values from the custard pi 2 board on plot.ly. The script is basically a combination
+# of two example scripts, namely (use these as reference):
+#  - https://github.com/plotly/workshop/tree/master/raspberry-pi/tmp36
+#  - http://www.sf-innovations.co.uk/downloads/cpi2_an_in.txt
+#
+# Usage: sudo python monitor.py OUTPUTFILE STREAMFLAG
+# Issues: stream.close() seems to be very slow on the RPi
 
 import sys
 import RPi.GPIO as GPIO
@@ -22,98 +18,106 @@ import json
 from sys import argv
 import os.path
 
-
+# set GPIOs so they read A/D values
 def initGPIO():
-	GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
 
-	GPIO.setup(24, GPIO.OUT)
-	GPIO.setup(23, GPIO.OUT)
-	GPIO.setup(19, GPIO.OUT)
-	GPIO.setup(21, GPIO.IN)
+    GPIO.setup(24, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(19, GPIO.OUT)
+    GPIO.setup(21, GPIO.IN)
 
-	GPIO.output(24, True)
-	GPIO.output(23, False)
-	GPIO.output(19, True)
+    GPIO.output(24, True)
+    GPIO.output(23, False)
+    GPIO.output(19, True)
 
-	GPIO.output(24, False)
+    GPIO.output(24, False)
 
-def initPlotly():
-	with open('./config.json') as config_file:
-	    	plotly_user_config = json.load(config_file)
-
-		py.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
-
-		url = py.plot([
-		{
-			'x': [], 'y': [], 'type': 'scatter',
-			'stream': {
-			    'token': plotly_user_config['plotly_streaming_tokens'][0],
-			    'maxpoints': 5000
-			}
-		    }], filename='hygro analog value', auto_open=False)
-
-		print "View your streaming graph here: ", url
-		stream = py.Stream(plotly_user_config['plotly_streaming_tokens'][0])
-		stream.open()
-
-		return stream
-
+# clean up GPIO handling
 def endGPIO():
-	GPIO.output(24, True)
-	GPIO.cleanup()
+    GPIO.output(24, True)
+    GPIO.cleanup()
 
+# use the json file for configuration, it should have this shape
+# {
+# "plotly_streaming_tokens": ["your_stream_token", "another_stream_token"],
+# "plotly_api_key": "your_plotly_api_key",
+# "plotly_username": "your_plotly_username"
+# }
+def initPlotly():
+    with open('./config.json') as config_file:
+        plotly_user_config = json.load(config_file)
+
+        py.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
+
+        url = py.plot([
+                          {
+                          'x': [], 'y': [], 'type': 'scatter',
+                          'stream': {
+                          'token': plotly_user_config['plotly_streaming_tokens'][0],
+                          'maxpoints': 5000
+                          }
+                          }], filename='hygro analog value', auto_open=False)
+
+        print "View your streaming graph here: ", url
+        stream = py.Stream(plotly_user_config['plotly_streaming_tokens'][0])
+        stream.open()
+
+        return stream
+
+# read the analog values
 def readAnalog():
-	anip=0
-	word1 = [1, 1, 1, 1, 1]
-	for x in range (0,5):
-		GPIO.output(19, word1[x])
-		time.sleep(0.01)
-		GPIO.output(23, True)
-		time.sleep(0.01)
-		GPIO.output(23, False)
+    anip = 0
+    word1 = [1, 1, 1, 1, 1]
+    for x in range(0, 5):
+        GPIO.output(19, word1[x])
+        time.sleep(0.01)
+        GPIO.output(23, True)
+        time.sleep(0.01)
+        GPIO.output(23, False)
 
-	for x in range (0,12):
-		GPIO.output(23,True)
-		time.sleep(0.01)
-		bit=GPIO.input(21)
-		time.sleep(0.01)
-		GPIO.output(23,False)
-		value=bit*2**(12-x-1)
-		anip=anip+value
-	return anip
+    for x in range(0, 12):
+        GPIO.output(23, True)
+        time.sleep(0.01)
+        bit = GPIO.input(21)
+        time.sleep(0.01)
+        GPIO.output(23, False)
+        value = bit * 2 ** (12 - x - 1)
+        anip = (anip + value) * 3.3 / 4096
+
+    return anip
 
 
-script, filename, streamit = argv
+def main():
+    script, filename, streamit = sys.argv
 
-if os.path.isfile(filename):
-	print "file already exists"
-	sys.exit()
+    # just protect yourself not to overwrite the data file
+    if os.path.isfile(filename):
+        print "file already exists"
+        sys.exit()
 
-target = open(filename, 'w')
-target.write("time, volts at custard\n")
-initGPIO()
-if int(streamit): 
-	stream = initPlotly()
-try:
-	while True:	
-		volt = readAnalog()*3.3/4096
-		if int(streamit):
-			stream.write({'x': datetime.datetime.now(), 'y': volt})
-		x = datetime.datetime.now()
-		print x, volt
-		target.write(str(x) + "; " + str(volt) + "\n")
-		time.sleep(1)
-		
-except KeyboardInterrupt:
-	print "stopping"
-	target.close()
-	stream.close()
-	endGPIO()
-	sys.exit()
+    target = open(filename, 'w')
+    target.write("time, volts at custard\n")
+    initGPIO()
 
-#TO TEST
-#Connect Channel 0 to 3.3V and voltage should be 3.3V
-#Connect two equal resistors in series from 3.3V to 0V.
-#Connect mid point of resistors to Channel 0 input
-#Voltage should be 1.65V
+    if int(streamit):
+        stream = initPlotly()
+    try:
+        while True:
+            volt = readAnalog()
+            if int(streamit):
+                stream.write({'x': datetime.datetime.now(), 'y': volt})
+            x = datetime.datetime.now()
+            print x, volt
+            target.write(str(x) + "; " + str(volt) + "\n")
+            time.sleep(1)
 
+    except KeyboardInterrupt:
+        print "stopping"
+        target.close()
+        stream.close()
+        endGPIO()
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
